989718b7a49e52a3f1ed7e95299f469c6d3ef36d
ArtVelych <artemvelychko1999@gmail.com>
2025-05-02 00:45:21 +0200

    Tasks 1-5

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,185 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from "react";
+import "./App.css";
 
-function App() {
-  const [count, setCount] = useState(0)
+// import axios from "axios";
+import { fetchArticlesWithTopic } from "./articles-api";
+
+import ArticleList from "./ArticleList/ArticleList";
+
+// ============================================== Task 1 ============================== //
+
+// const App = () => {
+//   const [articles, setArticles] = useState([]);
+
+//   useEffect(() => {
+//     async function fetchArticles() {
+//       const response = await axios.get(
+//         "https://hn.algolia.com/api/v1/search?query=react"
+//       );
+//       setArticles(response.data.hits);
+//     }
+
+//     fetchArticles();
+//   }, []);
+
+//   return (
+//     <div>
+//       <h1>Latest articles</h1>
+
+//       {articles.length > 0 && (
+//         <ul>
+//           {articles.map(({ objectID, url, title }) => (
+//             <li key={objectID}>
+//               <a href={url} target="_blank" rel="noreferrer noopener">
+//                 {title}
+//               </a>
+//             </li>
+//           ))}
+//         </ul>
+//       )}
+//     </div>
+//   );
+// };
+
+// export default App;
+
+// ============================================== Task 2: ArticleList component ============================== //
+// const App = () => {
+//   const [articles, setArticles] = useState([]);
+
+//   useEffect(() => {
+//     async function fetchArticles() {
+//       const response = await axios.get(
+//         "https://hn.algolia.com/api/v1/search?query=react"
+//       );
+//       setArticles(response.data.hits);
+//     }
+
+//     fetchArticles();
+//   }, []);
+
+//   return (
+//     <div>
+//       <h1>Latest articles</h1>
+//       {articles.length > 0 && <ArticleList items={articles} />}
+//     </div>
+//   );
+// };
+
+// export default App;
+
+// ============================================== Task 3: Loading ============================== //
+
+// const App = () => {
+//   const [articles, setArticles] = useState([]);
+//   const [loading, setLoading] = useState(false);
+
+//   useEffect(() => {
+//     async function fetchArticles() {
+//       try {
+//         // 1. Встановлюємо індикатор в true перед запитом
+//         setLoading(true);
+//         const response = await axios.get(
+//           "https://hn.algolia.com/api/v1/search?query=react"
+//         );
+//         setArticles(response.data.hits);
+//       } catch (error) {
+//         // Тут будемо обробляти помилку
+//       } finally {
+//         // 2. Встановлюємо індикатор в false після запиту
+//         setLoading(false);
+//       }
+//     }
+
+//     fetchArticles();
+//   }, []);
+
+//   return (
+//     <div>
+//       <h1>Latest articles</h1>
+//       {loading && <p>Loading data, please wait...</p>}
+//       {articles.length > 0 && <ArticleList items={articles} />}
+//     </div>
+//   );
+// };
+
+// export default App;
+
+// ============================================== Task 4: Loading error ============================== //
+
+// const App = () => {
+//   const [articles, setArticles] = useState([]);
+//   const [loading, setLoading] = useState(false);
+//   const [error, setError] = useState(false);
+
+//   useEffect(() => {
+//     async function fetchArticles() {
+//       try {
+//         setLoading(true);
+//         const response = await axios.get(
+//           "https://hn.algolia.com/api/v1/search1?query=react"
+//         );
+//         setArticles(response.data.hits);
+//       } catch (error) {
+//         // Встановлюємо стан error в true
+//         setError(true);
+//       } finally {
+//         setLoading(false);
+//       }
+//     }
+
+//     fetchArticles();
+//   }, []);
+
+//   return (
+//     <div>
+//       <h1>Latest articles</h1>
+//       {loading && <p style={{ fontSize: 20 }}>Loading data, please wait...</p>}
+//       {error && (
+//         <p style={{ fontSize: 20, color: "red" }}>
+//           Whoops, something went wrong! Please try reloading this page!
+//         </p>
+//       )}
+//       {articles.length > 0 && <ArticleList items={articles} />}
+//     </div>
+//   );
+// };
+
+// export default App;
+
+// ============================================== Task 5: Delegating responsibilities ============================== //
+export function App() {
+  const [articles, setArticles] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
+
+  useEffect(() => {
+    async function fetchArticles() {
+      try {
+        setLoading(true);
+        // 2. Використовуємо HTTP-функцію
+        const data = await fetchArticlesWithTopic("react");
+        setArticles(data);
+      } catch (error) {
+        setError(true);
+      } finally {
+        setLoading(false);
+      }
+    }
+
+    fetchArticles();
+  }, []);
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div>
+      <h1>Latest articles</h1>
+      {loading && <p>Loading data, please wait...</p>}
+      {error && (
+        <p>Whoops, something went wrong! Please try reloading this page!</p>
+      )}
+      {articles.length > 0 && <ArticleList items={articles} />}
+    </div>
+  );
 }
 
-export default App
+export default App;
